434be57be40eba4baf76801469ac2ca27399bffe
Kirill Kostiuk <35852223+YourFrieng@users.noreply.github.com>
2020-05-08 02:07:06 +0300

    Add sounds, sound script for hero, door and lamps,, make prefabs of door, lamps
    Add sounds of steps, open door, and on/off lamp, add sound script for hero, door and lamps,, make prefabs of door, lamps, fix bug with lamp animation

---
+++ Assets/Scripts/Make_sound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Make_sound : MonoBehaviour
+{
+    AudioSource myTrack;
+    private bool interactionAllowed;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            interactionAllowed = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            interactionAllowed = false;
+        }
+    }
+    void Start()
+    {
+        myTrack = GetComponent<AudioSource>();
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        if (interactionAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            myTrack.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Sound_For_Hero.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_For_Hero : MonoBehaviour
+{
+    private AudioSource myTrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTrack = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            myTrack.loop = true;
+            myTrack.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            myTrack.loop = false;
+        }
+    }
+}
