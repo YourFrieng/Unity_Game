db7fb9ef1290fa120da7d17a60c792ad38eb1bed
Manakir <46427131+Manakir@users.noreply.github.com>
2020-05-07 14:19:36 +0300

    small changes in dialogue system & npc movement
    player can't move when the dialogue is active, npc random move

--- Assets/Scripts/DialogueManager1.cs
+++ Assets/Scripts/DialogueManager1.cs
@@ -8,16 +8,23 @@ public class DialogueManager1 : MonoBehaviour
     public Queue<string> sentences;//list of future replicas
     private Queue<bool> isPlayerSentences;
 
+    public GameObject Player;
+    private Controller controller;
+    private Animator anim;
     public Dialogue dialogue;
     public GameObject dialogueBox;
     public Color playerColor;
     public Text dialogueText;
     bool n = true;//used to activate StartDialogue() function once
+    public npcMovement npcMovement;
     void Start()
     {
         dialogueBox.SetActive(false);
         sentences = new Queue<string>();
         isPlayerSentences = new Queue<bool>();
+        controller = Player.GetComponent<Controller>();
+        anim = Player.GetComponent<Animator>();
+        npcMovement = GetComponent<npcMovement>();
     }
     void OnTriggerStay2D(Collider2D col)//conditions to start dialogue
     {
@@ -36,6 +43,9 @@ public class DialogueManager1 : MonoBehaviour
     }
     public void StartDialogue()
     {
+        controller.enabled = false;
+        anim.enabled = false;
+        npcMovement.moveSpeed = 0;
         dialogueBox.SetActive(true);
         foreach (string sentence in dialogue.sentences)//copy replicas from Dialogue to Queue 
         {
@@ -70,5 +80,8 @@ public class DialogueManager1 : MonoBehaviour
     void EndDialogue()
     {
         dialogueBox.SetActive(false);
+        controller.enabled = true;
+        anim.enabled = true;
+        npcMovement.moveSpeed = 20;
     }
 }
---
+++ Assets/Scripts/npcMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb2d;
+
+    public bool isWalking;
+
+    public float walkTime;
+    private float walkCounter;
+
+    public float waitTime;
+    private float waitCounter;
+
+    private int walkDirection;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        walkCounter = walkTime;
+        waitCounter = waitTime;
+        ChooseDirection();
+    }
+
+    void Update()
+    {
+        if (isWalking)
+        {
+            walkCounter -= Time.deltaTime;
+            switch (walkDirection)
+            {
+                case 0:
+                    rb2d.velocity = new Vector2(0, moveSpeed);
+                    break;
+                case 1:
+                    rb2d.velocity = new Vector2(moveSpeed, 0);
+                    break;
+                case 2:
+                    rb2d.velocity = new Vector2(0, -moveSpeed);
+                    break;
+                case 3:
+                    rb2d.velocity = new Vector2(-moveSpeed, 0);
+                    break;
+            }
+            if (walkCounter < 0)
+            {
+                waitCounter = waitTime;
+                isWalking = false;
+            }
+        }
+        else
+        {
+            waitCounter -= Time.deltaTime;
+            rb2d.velocity = Vector2.zero;
+            if(waitCounter < 0)
+            {
+                ChooseDirection();
+            }
+        }
+    }
+    public void ChooseDirection()
+    {
+        walkDirection = Random.Range(0, 4);
+        isWalking = true;
+        walkCounter = walkTime;
+    }
+}
